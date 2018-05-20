2d65ade78bce12e0ca59faff3ab67783c85b512b
rob2013 <rbmcgill@hotmail.com>
2018-05-20 17:02:07 -0400

    adding i3 config

--- installed.sh
+++ installed.sh
@@ -7,4 +7,5 @@ yum install vim -y
 yum install terminator -y
 dnf install i3 i3status dmenu i3lock xbacklight feh -y
 #yum install bash-completion openvpn
-
+sudo dnf install spotify-client
+dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
